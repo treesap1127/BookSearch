6f05892856622a4b19408a72094b6b44b98c3724
jihunz <thespringsong@gmail.com>
2023-09-16 16:52:36 +0900

    chore: 알림 테스트

--- bookSearcher/src/main/java/com/core/book/service/BookServiceImpl.java
+++ bookSearcher/src/main/java/com/core/book/service/BookServiceImpl.java
@@ -50,8 +50,8 @@ public class BookServiceImpl implements BookService {
 
 	@Override
 	public SearchHits<Book> search(String keyword) {
+    	//test
 
-    	//알림 테스트
 //		NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
 //				.withQuery(QueryBuilders.functionScoreQuery(
 //						QueryBuilders.boolQuery()
