adbedf79f8e0513f1bb4a713f8bf62b681ee21fc
jihunz <thespringsong@gmail.com>
2023-09-26 23:55:38 +0900

    refactor: search 패키지 삭제

--- bookSearcher/src/main/java/com/core/module/utils/Indexing.java
+++
@@ -1,8 +0,0 @@
-package com.core.module.utils;
-
-import java.util.List;
-import java.util.Map;
-
-public interface Indexing<T> {
-	String Indexing(String string, List<Map<String, String>> list);
-}
