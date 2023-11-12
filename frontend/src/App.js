93f02067c81cca1b227c338810ba76a00df98fe7
jihunz <thespringsong@gmail.com>
2023-11-12 09:51:42 +0900

    build: 프론트엔드 폴더와 백엔드 폴더 분리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import MyComponent from './admin/components/MyComponent.js';
 
 const App = () => {
   return (
     <div className="app">
       <h1>index 페이지</h1>
-      <MyComponent />
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import from "react-dom/client" instead of "react-dom"
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Mybatis from './admin/components/Mybatis';
-import Search from './admin/components/Search';
-import Indexing from './admin/components/indexing';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -21,5 +17,3 @@ root.render(
     </React.StrictMode>
   </Router>
 );
-
-reportWebVitals();
