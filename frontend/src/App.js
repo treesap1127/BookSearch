7cbb86bb867368cf4c73ec9ccebc7a393a4eb784
jihunz <thespringsong@gmail.com>
2023-11-15 21:54:52 +0900

    feat: 자동 생성된 코드로 퍼블리싱 진행 중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import SearchPage from "./search/components/SearchPage";
 
 const App = () => {
   return (
     <div className="app">
       <h1>index 페이지</h1>
-        <SearchPage/>
     </div>
   );
 };
