e888ad79dd027631c7337b3533783a06f36e861d
jihunz <thespringsong@gmail.com>
2023-11-29 17:53:49 +0900

    chore: cors 문제 수정 중

--- backend/src/main/java/com/core/config/CorsConfig.java
+++ backend/src/main/java/com/core/config/CorsConfig.java
@@ -14,9 +14,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println(url);
         registry.addMapping("/api/**")
-                .allowedOrigins(url) // 허용할 오리진(도메인) 설정
+                .allowedOrigins("*") // 허용할 오리진(도메인) 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
-                .allowCredentials(true) // 쿠키를 허용할지 여부
+//                .allowCredentials(true) // 쿠키를 허용할지 여부
                 .maxAge(3600); // CORS preflight 요청의 유효 시간 설정 (초)
     }
 }
